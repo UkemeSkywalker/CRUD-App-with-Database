cbb23d330b17d56f3b7c3e07f84c7021ecf0c0df
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-05-13 01:56:08 +0100

    serving delopyment

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.delete("/delete/:id", deleteUser)
 
 // Create PORT
 const port = process.env.PORT || PORT;
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // Listen to connection
 app.listen(port, () => console.log(`app running on port ${port}`));
